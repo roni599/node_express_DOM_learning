9e31ba2f83fc151381d451cb1873e39038da912b
Ebrahim Khalil Roni <ekroni99@gmail.com>
2024-02-15 09:44:51 +0600

    install cors packege,added public folder and then create css folder and then create style.css and in views folder home.html and 404.html file created and use static middleware

--- lesson21/app.js
+++ lesson21/app.js
@@ -1,11 +1,23 @@
 const express=require('express');
 const app=express();
+const cors=require('cors');
+const path=require('path');
 const userRouter=require('./routes/users.route');
 const PORT=3000;
 const hostName=`127.0.0.1`;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/home.html'))
+})
+
+app.use('*',(req,res)=>{
+ res.sendFile(path.join(__dirname+'/views/404.html'));
+})
 
 module.exports={
     app,
--- lesson21/routes/users.route.js
+++ lesson21/routes/users.route.js
@@ -1,6 +1,7 @@
 const express=require('express');
-const { getUsers,postUsers } = require('../controllers/users.controller');
+const { getUsers,postUsers, getHome } = require('../controllers/users.controller');
 const router=express.Router();
+
 
 router.get('/users',getUsers);
 router.post('/users',postUsers)
